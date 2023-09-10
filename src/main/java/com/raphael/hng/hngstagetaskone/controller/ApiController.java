dbfe4f7f54eec928de274575f5b1dd697f0ad958
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-09-10 17:50:25 +0100

    updated commit

--- src/main/java/com/raphael/hng/hngstagetaskone/controller/ApiController.java
+++ src/main/java/com/raphael/hng/hngstagetaskone/controller/ApiController.java
@@ -17,20 +17,20 @@ public class ApiController {
 
     @GetMapping("/api")
     public ResponseEntity<ApiResponse> getInfo(
-            @RequestParam("slack_name") String slackName,
+            @RequestParam("slack_name") String slack_name,
             @RequestParam("track") String track
     ) {
 
         SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
-        String utc_time = dayFormat.format(new Date());
+        String current_day = dayFormat.format(new Date());
 
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-        String current_day = dateFormat.format(new Date());
+        String utc_time = dateFormat.format(new Date());
 
         String github_file_url = "https://github.com/raphaelekpei/hngTask-1/blob/master/src/main/java/com/raphael/hng/hngstagetaskone/controller/ApiController.java";
         String github_repo_url = "https://github.com/raphaelekpei/hngTask-1";
 
-        ApiResponse apiResponse = new ApiResponse(slackName, current_day, utc_time, track, github_file_url, github_repo_url, HttpStatus.OK.value());
+        ApiResponse apiResponse = new ApiResponse(slack_name, current_day, utc_time, track, github_file_url, github_repo_url, HttpStatus.OK.value());
         return ResponseEntity.ok(apiResponse);
 
     }
