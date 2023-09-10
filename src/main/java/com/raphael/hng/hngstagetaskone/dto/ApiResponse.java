a1275a43ce2350eb557f706f243274f089946860
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-09-10 18:07:17 +0100

    committed seven

--- src/main/java/com/raphael/hng/hngstagetaskone/dto/ApiResponse.java
+++ src/main/java/com/raphael/hng/hngstagetaskone/dto/ApiResponse.java
@@ -10,7 +10,7 @@ public class ApiResponse {
     private String current_day;
     private String utc_time;
     private String track;
-    private String github_file_Url;
+    private String github_file_url;
     private String github_repo_url;
     private Integer status_code;
 
